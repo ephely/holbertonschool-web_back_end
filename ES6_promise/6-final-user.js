5d0a5f462e177e962d37973e6608a61198761e78
« Ephely » <ephely1@gmail.com>
2025-04-03 10:33:31 +0200

    Fixed Task 2 & 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, filename) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(filename);
-  
+
   return Promise.allSettled([userPromise, photoPromise])
   .then((results) => results.map((result) => ({
     status: result.status,
