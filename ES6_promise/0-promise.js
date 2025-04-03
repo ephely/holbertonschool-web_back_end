7a756626a745cb673d519572b43a553dace974a1
« Ephely » <ephely1@gmail.com>
2025-04-03 10:16:00 +0200

    Fixed Task 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    if (success) {
-        resolve({"Success"});
-    } else {
-      reject(new Error("API request failed"));
-    }
-  }
-  );
+  return new Promise((resolve) => {
+    resolve('Success');
+  });
 }
