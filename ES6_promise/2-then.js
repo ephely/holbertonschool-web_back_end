1c3457ed8c64ccf588178d9e129051779f74f2e1
« Ephely » <ephely1@gmail.com>
2025-04-03 10:30:19 +0200

    Fixed Task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,13 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject)
+  return promise
   .then(() => {
-    return { status: 200, body: "success" };
+    return {status: 200, body: 'success'};
   })
   .catch(() => {
     return new Error();
   })
   .finally(() => {
-    console.log("Got a response from the API");
+    console.log('Got a response from the API');
   })
 }
   
