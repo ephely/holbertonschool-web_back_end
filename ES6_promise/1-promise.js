dda75b569d06d6a27f869179dd04b791bcb3d6cb
« Ephely » <ephely1@gmail.com>
2025-04-04 12:13:45 +0200

    Deleted an unnecessary line

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,6 +1,5 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    success = true;
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
