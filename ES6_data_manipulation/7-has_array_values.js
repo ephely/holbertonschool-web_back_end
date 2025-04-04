0e1b7309e2d10088b4a2dcb1997fbaccfc03cc45
« Ephely » <ephely1@gmail.com>
2025-04-04 12:06:31 +0200

    Fix Task 7

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,6 @@
 export default function hasValuesFromArray(set, array) {
   if (!(set instanceof Set)) {
-    throw new Error("The argument must be a Set");
+    throw new Error('The argument must be a Set');
   }
   if (!Array.isArray(array)) {
     return [];
