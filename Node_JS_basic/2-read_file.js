429ce768cc9d0aacee78451dfd70738a68b26992
« Ephely » <ephely1@gmail.com>
2025-05-23 23:10:20 +0200

    Changes

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,40 +2,45 @@
 const fs = require('fs');
 
 function countStudents(path) {
+  let data;
   try {
-    const data = fs.readFileSync(path, 'utf-8');
-
-    const lines = data.split('\n');
-    const validLines = lines.filter((line) => line.trim() !== '');
-    const header = validLines[0].split(',');
-    const students = validLines.slice(1);
-
-    const fields = {};
-
-    students.forEach((line) => {
-      const parts = line.split(',');
-      if (parts.length === header.length) {
-        const firstname = parts[0].trim();
-        const field = parts[parts.length - 1].trim();
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-      }
-    });
-
-    const totalStudents = students.length;
-    console.log(`Number of students: ${totalStudents}`);
-
-    for (const field in fields) {
-      console.log(
-        `Number of students in ${field}: ${
-          fields[field].length
-        }. List: ${fields[field].join(', ')}`,
-      );
-    }
-  } catch (err) {
+    data = fs.readFileSync(path, 'utf-8');
+  } catch (error) {
     throw new Error('Cannot load the database');
+  }
+
+  const lines = data.split('\n').filter((line) => line.trim() !== '');
+  if (lines.length < 2) {
+    console.log('Number of students: 0');
+    return;
+  }
+
+  const header = lines[0].split(',');
+  const students = lines.slice(1);
+
+  const fields = {};
+  let totalStudents = 0;
+
+  students.forEach((line) => {
+    const student = line.split(',');
+    if (student.length === header.length) {
+      const field = student[header.length - 1];
+      const firstName = student[0];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+      totalStudents += 1;
+    }
+  });
+
+  console.log(`Number of students: ${totalStudents}`);
+  for (const field in fields) {
+    console.log(
+      `Number of students in ${field}: ${fields[field].length}. List: ${fields[
+        field
+      ].join(', ')}`,
+    );
   }
 }
 
