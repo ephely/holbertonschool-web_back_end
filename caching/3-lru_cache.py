5cebd1fd212d4c3d212321e43c50a2e74d07e331
« Ephely » <ephely1@gmail.com>
2026-02-23 11:22:15 +0100

    Task 3

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache """
+
+    def put(self, key, item):
+        """ Assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard = list(self.cache_data.keys())[0]
+            print("DISCARD: {}".format(discard))
+            del self.cache_data[discard]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Return the value in self.cache_data
+        linked to key.
+        """
+        if key is None:
+            return None
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            del self.cache_data[key]
+            self.cache_data[key] = value
+            return value
+        return None
