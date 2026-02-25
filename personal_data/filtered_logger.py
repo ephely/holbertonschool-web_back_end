c903d5d152f959e87b102981067c2b3e1115056b
« Ephely » <ephely1@gmail.com>
2026-02-25 15:39:16 +0100

    Task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,27 @@
 """ Regex-ing
 """
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        for field in self.fields:
+            message = re.sub(r"({})=([^{}]*)".format(field, self.SEPARATOR),
+                             r"\1={}".format(self.REDACTION), message)
+        return message
 
 
 def filter_datum(fields, redaction, message, separator):
