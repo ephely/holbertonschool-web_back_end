f2e53d06fafda600534f1fd89c72ecc9e89ad0a7
« Ephely » <ephely1@gmail.com>
2026-02-25 15:46:07 +0100

    Test

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,20 @@
-#!/usr/bin/python3
-""" Regex-ing
+#!/usr/bin/env python3
+"""
+Filtered loggers
 """
 import re
 import logging
+from typing import List
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Returns the log message obfuscated """
+    return re.sub(r"({})=([^{}]*)".format('|'.join(fields), separator),
+                  r"\1={}".format(redaction), message)
 
 
 class RedactingFormatter(logging.Formatter):
@@ -13,19 +25,24 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        message = super().format(record)
-        for field in self.fields:
-            message = re.sub(r"({})=([^{}]*)".format(field, self.SEPARATOR),
-                             r"\1={}".format(self.REDACTION), message)
-        return message
+        """ Filters values in incoming log records using filter_datum """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
-def filter_datum(fields, redaction, message, separator):
-    """ Returns the log message obfuscated """
-    return re.sub(r"({})=([^{}]*)".format('|'.join(fields), separator),
-                  r"\1={}".format(redaction), message)
+def get_logger() -> logging.Logger:
+    """ Returns a logging.Logger object """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
