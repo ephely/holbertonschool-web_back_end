a6970096ce802173ca0d7f4345c9b5f467fda31d
« Ephely » <ephely1@gmail.com>
2026-02-28 00:09:45 +0100

    Task 3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Authentication module for the API
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class to manage API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method to check if authentication is required for a path
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Method to get the authorization header from the request
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method to get the current user from the request
+        """
+        return None
