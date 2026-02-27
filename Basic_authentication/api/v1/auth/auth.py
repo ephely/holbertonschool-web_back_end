0e7067f565c0f508b1a8ba7b694601f8e82c7760
« Ephely » <ephely1@gmail.com>
2026-02-28 00:19:15 +0100

    Task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,17 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method to check if authentication is required for a path
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path.endswith('/'):
+            check_path = path
+        else:
+            check_path = path + '/'
+        if check_path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to get the authorization header from the request
