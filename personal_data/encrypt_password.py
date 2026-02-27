a4756cd6504850ab239be2fb4282488a7a8166ff
« Ephely » <ephely1@gmail.com>
2026-02-27 10:55:53 +0100

    Task 5

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password with a salt using bcrypt
+    """
+    password_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+
+    return hashed_password
