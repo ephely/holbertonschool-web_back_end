72f6b54e439f1d9db7c1af2a432a87aac0e12f0d
« Ephely » <ephely1@gmail.com>
2026-04-17 21:40:15 +0200

    Task 10

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -48,3 +48,14 @@ class Auth:
                                   user.hashed_password.encode('utf-8'))
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Generate a new session ID for a user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
