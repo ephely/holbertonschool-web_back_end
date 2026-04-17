64767a5ba75abed623d8b8a48f2670e606a75d68
« Ephely » <ephely1@gmail.com>
2026-04-17 22:33:33 +0200

    Task 16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -75,3 +75,14 @@ class Auth:
         """ Log out a user
         """
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generate a reset password token for a user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
