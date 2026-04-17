8bf6b2001da0f090be397ce90b708d8710a8c7f7
« Ephely » <ephely1@gmail.com>
2026-04-17 12:15:53 +0200

    Task 4

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ Auth module
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -34,3 +35,11 @@ class Auth:
         """current_user
         """
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
