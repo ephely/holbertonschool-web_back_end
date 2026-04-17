591302628c12a6034d84c5b476d7c4e633fae3e9
« Ephely » <ephely1@gmail.com>
2026-04-17 22:06:05 +0200

    Task 13

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -70,3 +70,8 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Log out a user
+        """
+        self._db.update_user(user_id, session_id=None)
