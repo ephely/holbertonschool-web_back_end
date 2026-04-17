a0e4287fc57ea527a39a1fa92101fb5cb1e5d4f8
« Ephely » <ephely1@gmail.com>
2026-04-17 22:50:59 +0200

    Task 17-19

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -86,3 +86,14 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update the user password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_pwd = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_pwd.decode('utf-8'),
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
