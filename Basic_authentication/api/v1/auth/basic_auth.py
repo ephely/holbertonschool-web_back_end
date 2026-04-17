5712a1291bd93021286b2c581dd32cd6fce56e15
« Ephely » <ephely1@gmail.com>
2026-04-17 10:08:46 +0200

    Task 9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,3 +35,15 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """extract_user_credentials
+        """
+        if decoded_base64_authorization_header is None or \
+           type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        return tuple(decoded_base64_authorization_header.split(':', 1))
