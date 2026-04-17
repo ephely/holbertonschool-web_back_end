101451171a2b181357d6abfad3643277ba68f3f5
« Ephely » <ephely1@gmail.com>
2026-04-17 21:34:31 +0200

    Task 8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -31,3 +31,13 @@ class Auth:
         except NoResultFound:
             hashed_pwd = _hash_password(password)
             return self._db.add_user(email, hashed_pwd.decode('utf-8'))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Check if a login is valid
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password.encode('utf-8'))
+        except NoResultFound:
+            return False
