fdc340c87e28fc424b24905f302a9cfb1e2106b5
« Ephely » <ephely1@gmail.com>
2026-04-17 10:12:47 +0200

    Task 10

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Basic authentication module
 """
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -47,3 +49,21 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         return tuple(decoded_base64_authorization_header.split(':', 1))
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """user_object_from_credentials
+        """
+        if type(user_email) is not str or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        if not users or len(users) == 0:
+            return None
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
--- Basic_authentication/models/user.py
+++ Basic_authentication/models/user.py
@@ -32,3 +32,12 @@ class User(Base):
         if self.first_name is None:
             return f"{self.last_name}"
         return f"{self.first_name} {self.last_name}"
+
+    def is_valid_password(self, pwd: str) -> bool:
+        """ Validate password """
+        if pwd is None or type(pwd) is not str:
+            return False
+        if self.password is None:
+            return False
+        import hashlib
+        return self.password == hashlib.md5(pwd.encode()).hexdigest().lower() or self.password == pwd
