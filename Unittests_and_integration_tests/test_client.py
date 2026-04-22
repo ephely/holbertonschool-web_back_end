a3f884a504c33d58e3e1a893943531a683cf5873
« Ephely » <ephely1@gmail.com>
2026-04-22 16:03:40 +0200

    Task 5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -21,3 +21,11 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
         )
+
+    def test_public_repos_url(self):
+        """ Test GithubOrgClient._public_repos_url """
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {"repos_url": "test_url"}
+            client = GithubOrgClient("test_org")
+            self.assertEqual(client._public_repos_url, "test_url")
