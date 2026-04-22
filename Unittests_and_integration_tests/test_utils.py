70a786c83bf64bc3e1840eab673e2c3dbb0a4cdb
« Ephely » <ephely1@gmail.com>
2026-04-22 11:28:28 +0200

    Updated Task 2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@ Unit tests for utils.access_nested_map
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
