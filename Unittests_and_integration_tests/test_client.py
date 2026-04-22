df03982ae8e6c44e537511403fa99f2de7992220
« Ephely » <ephely1@gmail.com>
2026-04-22 15:54:06 +0200

    Task 4

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Unit tests for GithubOrgClient """
+import unittest
+from unittest.mock import patch, Mock, PropertyMock
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Tests for GithubOrgClient class """
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        """ Test GithubOrgClient.org returns correct value """
+        client = GithubOrgClient(org_name)
+        client.org
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
