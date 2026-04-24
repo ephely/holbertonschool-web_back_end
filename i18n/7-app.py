9bfdd51d3184410b29966368b21834d275a9f527
« Ephely » <ephely1@gmail.com>
2026-04-24 10:53:21 +0200

    Task 7

---
+++ i18n/7-app.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+""" Use user locale """
+from pytz import UTC
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+import pytz
+
+
+class Config:
+    """ Configuration for Babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel()
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """ Retrieve user from mock database """
+    login_as = request.args.get('login_as')
+    if login_as:
+        try:
+            return users.get(int(login_as))
+        except (ValueError, TypeError):
+            return None
+    return None
+
+
+@app.before_request
+def before_request():
+    """ Detect user before request """
+    g.user = get_user()
+
+
+def get_locale():
+    """ Determine the best match with our supported languages """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user.get('locale')
+    header_locale = request.accept_languages.best_match(app.config['LANGUAGES'])
+    if header_locale:
+        return header_locale
+    return app.config['BABEL_DEFAULT_LOCALE']
+
+def get_timezone():
+    """ Determine the best timezone """
+    timezone = request.args.get('timezone')
+    if timezone in g.user.get('timezone'):
+        return timezone
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+    try:
+        return pytz.timezone(UTC).zone
+    except pytz.exceptions.UnknownTimeZoneError:
+        pass
+
+
+babel.init_app(app, locale_selector=get_locale, timezone_selector=get_timezone)
+
+
+@app.route('/')
+def index():
+    """ Basic route """
+    return render_template('7-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
