e54b784b876d36b5bb3bdbee79543d2b5b503111
« Ephely » <ephely1@gmail.com>
2026-06-19 19:42:49 +0200

    Task 4

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,6 +7,7 @@ import uuid
 from typing import Union, Callable, Optional
 from functools import wraps
 
+
 def count_calls(method: Callable) -> Callable:
     """Decorator to count how many times a method is called"""
     @wraps(method)
@@ -17,6 +18,45 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
+def call_history(method: Callable) -> Callable:
+    """Decorator to store the history of inputs and outputs"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper for call_history"""
+        input_key = "{}:inputs".format(method.__qualname__)
+        output_key = "{}:outputs".format(method.__qualname__)
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+        return output
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """Display the history of calls of a particular function"""
+    method_name = method.__qualname__
+    r = redis.Redis()
+
+    count = r.get(method_name)
+    if count is None:
+        count = 0
+    else:
+        count = int(count.decode('utf-8'))
+
+    print("{} was called {} times:".format(method_name, count))
+
+    inputs = r.lrange("{}:inputs".format(method_name), 0, -1)
+    outputs = r.lrange("{}:outputs".format(method_name), 0, -1)
+
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(
+            method_name,
+            i.decode('utf-8'),
+            o.decode('utf-8')
+        ))
+
+
 class Cache:
     """Cache class using Redis"""
     def __init__(self):
@@ -25,6 +65,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in Redis using a random key
@@ -55,6 +96,7 @@ class Cache:
         Get data from Redis and return as a string
         """
         return self.get(key, fn=lambda d: d.decode("utf-8"))
+
     def get_int(self, key: str) -> Optional[int]:
         """
         Get data from Redis and return as an int
--- 0x0B_redis_basic/main.py
+++ 0x0B_redis_basic/main.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
-""" Main file """
-
 Cache = __import__('exercise').Cache
-
 cache = Cache()
-
-cache.store(b"first")
-print(cache.get(cache.store.__qualname__))
-
-cache.store(b"second")
-cache.store(b"third")
-print(cache.get(cache.store.__qualname__))
+cache.store("foo")
+cache.store("bar")
+cache.store(42)
+replay = __import__('exercise').replay
+replay(cache.store)
