2ca4115a6c3193d06b8dc38c9c33c2cd3dbbdb15
« Ephely » <ephely1@gmail.com>
2026-07-17 15:55:56 +0200

    Task 4

---
+++ unittests_in_js/4-payment.test.js
@@ -0,0 +1,22 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should call Utils.calculateNumber with SUM, 100, 20, and log the correct output', function () {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledOnce).to.be.true;
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+});
