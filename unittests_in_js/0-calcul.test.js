e4b01b30964b098e7fa1794b39105fcaf5c386de
« Ephely » <ephely1@gmail.com>
2026-07-17 09:52:43 +0200

    Task 0 & 1

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,17 +1,17 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', function() {
-  it('should return 4 when adding 1 and 3', function() {
+describe('calculateNumber', function () {
+  it('should return 4 when adding 1 and 3', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it('should return 5 when adding 1 and 3.7', function() {
+  it('should return 5 when adding 1 and 3.7', function () {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-  it('should return 5 when adding 1.2 and 3.7', function() {
+  it('should return 5 when adding 1.2 and 3.7', function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-  it('should return 6 when adding 1.5 and 3.7', function() {
+  it('should return 6 when adding 1.5 and 3.7', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
